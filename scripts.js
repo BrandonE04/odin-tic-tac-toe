6cd1ac7780cab2e025e1049874321dd4042dfa43
BrandonEmbleton <brandonembleton04@gmail.com>
2025-06-19 04:21:22 -0400

    Change to classes

--- scripts.js
+++ scripts.js
@@ -1,73 +1,19 @@
-function createGame(){
-    const ROWNUMBER = 3;
-    const COLUMNNUMBER = 3;
 
-    let scoreboard = [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
 
-    const printScoreboard = () => {
-        console.log(scoreboard);
-    }
+class createGame{
 
-    const checkWin = () => {
-        let count = 1;
-
-        for(let i = 0; i < ROWNUMBER; i++){
-            count = 1;
-            for(let o = 1; o < ROWNUMBER; o++){
-                if((scoreboard[i][o-1] === scoreboard[i][o]) && scoreboard[i][o] !== null){
-                    count++;
-                }
-            }
-            if(count === 3){
-                return true;
-            }
-        }
-
-        for(let i = 0; i < COLUMNNUMBER; i++){
-            count = 1;
-            for(let o = 1; o < COLUMNNUMBER; o++){
-                if((scoreboard[o][i] === scoreboard[o-1][i]) && scoreboard[o][i] !== null){
-                    count++;
-                }
-            }
-            if(count === 3){
-                return true;
-            }
-        }
-        
-        if(scoreboard[0][0] === scoreboard[1][1]){
-            if((scoreboard[2][2] === scoreboard[1][1]) && scoreboard[1][1] !== null){
-                return true;
-            }
-        }
-
-        
-        if(scoreboard[0][2] === scoreboard[1][1]){
-            if((scoreboard[2][0] === scoreboard[1][1]) && scoreboard[1][1] !== null){
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    function Player(name, symbol){
+    Player(name, symbol){
         let playerName = name;
         let playerSymbol = symbol;
         const getName = () => {return playerName};
         const setName = (name) => {playerName = name};
 
         const takeTurn = (x,y,box) => {
-            if(scoreboard[x][y] === null){
+            if(this.scoreboard[x][y] === null){
                 const symbolBox = document.createElement("p");
                 symbolBox.setAttribute("class", "symbol");
                 symbolBox.textContent = symbol;
-                scoreboard[x][y] = symbol;
+                this.scoreboard[x][y] = symbol;
                 box.appendChild(symbolBox);
             } 
         }
@@ -75,80 +21,141 @@ function createGame(){
         return {getName, setName, takeTurn};
     }
 
-    function reset(){
-        symbols = document.querySelectorAll(".symbol");
+    constructor()   {
+        this.ROWNUMBER = 3;
+        this.COLUMNNUMBER = 3;
 
-        scoreboard = [
+        this.scoreboard = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+
+        this.playerOneName = "One";
+        this.playerTwoName = "Two";
+
+        this.playerOne = this.Player(this.playerOneName, "X", this.scoreboard);
+        this.playerTwo = this.Player(this.playerTwoName, "O", this.scoreboard);
+    }
+    
+    printScoreboard = () => {
+        console.log(this.scoreboard);
+    }
+
+    checkWin = () => {
+        let count = 1;
+
+        for(let i = 0; i < this.ROWNUMBER; i++){
+            count = 1;
+            for(let o = 1; o < this.ROWNUMBER; o++){
+                if((this.scoreboard[i][o-1] === this.scoreboard[i][o]) && this.scoreboard[i][o] !== null){
+                    count++;
+                }
+            }
+            if(count === 3){
+                return true;
+            }
+        }
+
+        for(let i = 0; i < this.COLUMNNUMBER; i++){
+            count = 1;
+            for(let o = 1; o < this.COLUMNNUMBER; o++){
+                if((this.scoreboard[o][i] === this.scoreboard[o-1][i]) && this.scoreboard[o][i] !== null){
+                    count++;
+                }
+            }
+            if(count === 3){
+                return true;
+            }
+        }
+        
+        if(this.scoreboard[0][0] === this.scoreboard[1][1]){
+            if((this.scoreboard[2][2] === this.scoreboard[1][1]) && this.scoreboard[1][1] !== null){
+                return true;
+            }
+        }
+
+        
+        if(this.scoreboard[0][2] === this.scoreboard[1][1]){
+            if((this.scoreboard[2][0] === this.scoreboard[1][1]) && this.scoreboard[1][1] !== null){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    reset = () => {
+        let symbols = document.querySelectorAll(".symbol");
+
+        this.scoreboard = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
         ];
 
-        for(symbol of symbols){
+        for(let symbol of symbols){
             symbol.parentNode.removeChild(symbol);
         }
     }
 
-    let playerOneName = "One";
-    let playerTwoName = "Two";
-
-    function namePlayers(){
-        playerOne.setName(document.forms["playerNames"]["playerOneName"].value);
-        playerTwo.setName(document.forms["playerNames"]["playerTwoName"].value); 
+    namePlayers(){
+        this.playerOne.setName(document.forms["playerNames"]["playerOneName"].value);
+        this.playerTwo.setName(document.forms["playerNames"]["playerTwoName"].value); 
 
         return false;
     }
       
-    const playerOne = Player(playerOneName, "X");
-    const playerTwo = Player(playerTwoName, "O");
-
-    return{playerOne, playerTwo, printScoreboard, checkWin, reset, namePlayers}
+    //return{playerOne, playerTwo, printScoreboard, checkWin, reset, namePlayers}
 }
 
-function playGame(){
-    const game = createGame();
-    const boxes = document.querySelectorAll(".box");
-    const display = document.querySelector(".display");
-    const reset = document.querySelector(".reset");
+class playGame{
+    constructor(){
+        this.game =  new createGame();
+        this.boxes = document.querySelectorAll(".box");
+        this.display = document.querySelector(".display");
+        this.reset = document.querySelector(".reset");
 
-    for(box of boxes){
-        box.addEventListener("click", playTurn)
+        for(let box of this.boxes){
+            box.addEventListener("click", this.playTurn)
+        }
+
+        this.reset.addEventListener("click", this.game.reset);
+
+        this.oddTurn = true;
+
     }
+    
+    playTurn = () => {
+        if(this.oddTurn){
+            this.game.playerOne.takeTurn(event.target.getAttribute("x"), event.target.getAttribute("y"), event.target);
+            this.oddTurn = false;
 
-    reset.addEventListener("click", game.reset);
-
-    let oddTurn = true;
-
-    function playTurn(){
-        if(oddTurn){
-            game.playerOne.takeTurn(this.getAttribute("x"), this.getAttribute("y"), this);
-            oddTurn = false;
-
-            if(game.checkWin()){
-                display.innerText = game.playerOne.getName() + " wins!";
+            if(this.game.checkWin()){
+                this.display.innerText = this.game.playerOne.getName() + " wins!";
             }
             else{
-                display.innerText = game.playerTwo.getName() + "'s Turn";
+                this.display.innerText = this.game.playerTwo.getName() + "'s Turn";
             }
         }
         else{
-            game.playerTwo.takeTurn(this.getAttribute("x"), this.getAttribute("y"), this);
-            oddTurn = true;
+            this.game.playerTwo.takeTurn(event.target.getAttribute("x"), event.target.getAttribute("y"), event.target);
+            this.oddTurn = true;
 
-            if(game.checkWin()){
-                display.innerText = game.playerTwo.getName() + " wins!";
+            if(this.game.checkWin()){
+                this.display.innerText = this.game.playerTwo.getName() + " wins!";
             }
             else{
-                display.innerText = game.playerOne.getName() + "'s Turn";
+                this.display.innerText = this.game.playerOne.getName() + "'s Turn";
             }
         }
         
     }
 
-    return game;
 }
 
-const game = playGame();
+const game = new playGame();
 
 
 
